d29901edf25cf8695fcc120a2cf9b1e9a8dddb42
jay005-ux <83492746+jay005-ux@users.noreply.github.com>
2026-06-04 13:40:01 +0530

    Update app.config.js

--- mobile-app/app.config.js
+++ mobile-app/app.config.js
@@ -3,7 +3,7 @@ const appJson = require("./app.json");
 const apiBaseUrl =
   process.env.EXPO_PUBLIC_API_BASE_URL ||
   appJson.expo.extra?.apiBaseUrl ||
-  "http://192.168.18.73:8000/api/";
+  "http://54.174.213.77:8000/api/";
 
 module.exports = {
   expo: {
